6c42281ebc3f182f6a372264a0626925109925b4
Igor <fullstack@naladka.su>
2023-02-08 19:35:22 +0300

    edit task1

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int ReadInt1(string argument)
+{
+	Console.Write($"Введите {argument}: ");
+	int number1;
+
+	while (!int.TryParse(Console.ReadLine(), out number1))
+	{
+		Console.WriteLine("Error! Try again.");
+	}
+
+	return number1;
+}
+
+int ReadInt2(string argument)
+{
+	Console.Write($"Введите {argument}: ");
+	int number2;
+
+	while (!int.TryParse(Console.ReadLine(), out number2))
+	{
+		Console.WriteLine("Error! Try again.");
+	}
+
+	return number2;
+}
+
+int MatDegree(int number1, int number2)
+{
+	int i = 1;
+   int result = number1; 
+	while(i < number2)
+	{
+	result = result * number1;
+	i++;
+	}
+	System.Console.WriteLine(result);
+
+	return result;
+}
+
+MatDegree(ReadInt1("число которое нужно возвести в степень"), ReadInt2("степень в которую нужно возвести число"));
