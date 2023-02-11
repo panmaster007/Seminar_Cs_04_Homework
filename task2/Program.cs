ef6ae27b26e70ced90804a32e52933e688c68cff
Igor <fullstack@naladka.su>
2023-02-11 21:48:20 +0300

    edit task2

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int ReadInt(string text)
+{
+	Console.Write(text);
+	int number;
+
+	while (!int.TryParse(Console.ReadLine(), out number))
+	{
+		Console.WriteLine("Error! Try again.");
+	}
+
+	return number;
+}
+
+int SumDigits(int number)
+{
+	int sum = 0;
+	while (number > 0)
+	{
+		sum = sum + (number % 10);
+		number = number / 10;
+	} 
+	return sum;
+}
+
+int number = ReadInt("Input number: ");
+Console.WriteLine($"the sum of all the digits in the number {number} = {SumDigits(number)}");
